2d5d34983a5f9514d35f868d7e56b3eee165079a
Pragati1109 <61589317+Pragati1109@users.noreply.github.com>
2020-07-04 00:51:18 +0530

    Create database if not exist added

--- register_.php
+++ register_.php
@@ -2,12 +2,27 @@
 
 session_start();
 
-header('location: login.php');
+$con = mysqli_connect('localhost', 'root', '');
+if (!$con) {
+  die('Not connected : ' . mysql_error());
+}
 
-$con = mysqli_connect('localhost','root');
+$db_selected = mysqli_select_db($con,'MM');
+if (!$db_selected) {
+ $p = "create database MM";
+ mysqli_query($con,$p);
+}
 
-mysqli_select_db($con,'budgettracker');
+mysqli_select_db($con,"MM");
 
+$table = "select * from userdata";
+$table_ = mysqli_query($con,$table);
+if(!$table_){
+    $create = "create table userdata(name varchar(255) not null, email varchar(255) not null, password varchar(255) not null, contact int(10) not null, city varchar(255) not null, address varchar(255) not null)";
+    mysqli_query($con,$create);
+}
+
+        
 $name = $_POST['name'];
 $mail = $_POST['email'];
 $pass = $_POST['password'];
@@ -22,11 +37,12 @@ $result = mysqli_query($con,$s);
 $num = mysqli_num_rows($result);
 
 if($num == 1){
-    echo "<h1>Username taken.</h1>";
+    echo "<h1><center>Username taken.<center></h1>";
+    echo "<a href = 'signup.php'><center>Click here to return to Signup page.<center></a>";
 }else{
     $reg = "insert into userdata(name,email,password,contact,city,address) values('$name','$mail','$pass','$contact','$city','$address')";
     mysqli_query($con,$reg);
-    echo "<h1>Successfully registered.</h1>";
+    header('location: login.php');
 }
 
 ?>
