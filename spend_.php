1d31afc4b8a88bf7498c385afc4cc9ae4c8925a7
Pragati1109 <61589317+Pragati1109@users.noreply.github.com>
2020-07-04 00:52:22 +0530

    create table if not exist added

--- spend_.php
+++ spend_.php
@@ -2,16 +2,27 @@
 
 session_start();
 
-header('location: spendings.php');
+$con = mysqli_connect('localhost', 'root', '');
+if (!$con) {
+  die('Not connected : ' . mysql_error());
+}
 
-$con = mysqli_connect('localhost','root','');
-
-mysqli_select_db($con,'budgettracker');
+mysqli_select_db($con,'MM');
 
 $title = $_POST['title'];
 $amount = $_POST['amount'];
 
-$entry = "insert into spend('title','amount') values('$title','$amount')";
+$tabl = "select * from spend";
+$tabl_ = mysqli_query($con,$tabl);
+if(!$tabl_){
+    $creat = "create table spend(name varchar(255) not null, title varchar(255) not null, amount int(255) not null)";
+    mysqli_query($con,$creat);
+}
+
+$name = $_SESSION['username'];
+$entry = "insert into spend(name,title,amount) values('$name','$title','$amount')";
 mysqli_query($con,$entry);
+
+header('location: spendings.php');
 
 ?>
