656af63cef90b6c59878d737e9528fd24521bce5
Pragati1109 <61589317+Pragati1109@users.noreply.github.com>
2020-07-05 10:41:37 +0530

    initial commit

---
+++ budget.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Budget</title>
+        <?php
+        include "sub/head.php";
+        ?>
+    </head>
+    
+    <body>
+        
+        <?php
+        include "sub/navbar2.php";
+        ?>
+        
+        <div class="container bottom" style='position:absolute; top: 100px;left:10px; justify-content: center; align-items: center; align-items: center !important'>
+            <div>
+                
+                <div class="col-lg-4">
+                    <div class="thumbnail">
+                        <div class="caption"> 
+                            <h1><center>To set the spending limit click on the image below.<center></h1>
+                        </div>
+                        <a href='#' id='popup'><img src="img/BudgetTracker.png"></a>
+                    </div>
+                </div>
+    </body>
+</html>
