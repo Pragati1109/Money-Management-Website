e0eb489d36a6973c51125725017363fdddaae9c6
Pragati1109 <61589317+Pragati1109@users.noreply.github.com>
2020-07-05 10:43:11 +0530

    date col added

--- spend_.php
+++ spend_.php
@@ -2,7 +2,8 @@
 
 session_start();
 
-$con = mysqli_connect('localhost', 'root', '');
+include "sub/con1.php";
+
 if (!$con) {
   die('Not connected : ' . mysql_error());
 }
@@ -11,13 +12,6 @@ mysqli_select_db($con,'MM');
 
 $title = $_POST['title'];
 $amount = $_POST['amount'];
-
-$tabl = "select * from spend";
-$tabl_ = mysqli_query($con,$tabl);
-if(!$tabl_){
-    $creat = "create table spend(name varchar(255) not null, title varchar(255) not null, amount int(255) not null)";
-    mysqli_query($con,$creat);
-}
 
 $name = $_SESSION['username'];
 $entry = "insert into spend(name,title,amount) values('$name','$title','$amount')";
